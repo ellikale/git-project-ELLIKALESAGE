96eaf749e3eb382802b885c6010d3ae123c77193
Ellika LeSage <Elesage1@hwemail.com>
2025-09-24 12:36:47 -0700

    Adds basic git directory creation
    Creates the foundation for a simplified Git implementation by setting up the initial directory structure.
    
    This allows to initialize the "git" and "git/objects" directories, which are essential for storing repository data.

--- Git.java
+++ Git.java
@@ -1,5 +1,17 @@
+import java.io.File;
+
 public class Git{
     public static void main(String[] args) {
         
+    }
+
+    public static void createGitDirectory(){
+        File git = new File("git");
+        git.mkdir();
+    }
+
+    public static void createObjectsDirectory(){
+        File git = new File("git/objects");
+        git.mkdir();
     }
 }
